abaefe294d529323abd32bde12d65ebce58c70d7
akshay <akshay@gmail.com>
2023-09-07 20:50:43 +0530

    requirements2

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'TRUE'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
