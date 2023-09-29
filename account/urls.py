d573f8952e686ae700dbd40dfd7b6a2e11226208
akshay <akshay@gmail.com>
2023-09-29 13:49:49 +0530

    password list

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.urls import path, include
+
 from django.contrib.auth import views as auth_views
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 urlpatterns = [
